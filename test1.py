127f39121b7aa2164db8f99973650ff6f313fd04
kasing <kasingzzz@gmail.com>
2023-05-22 01:09:40 +0200

    added one line of code

--- test1.py
+++ test1.py
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 def nagasaki (hi):
     hey = 3
     hey = hey + 32 + 0.2
     hey = int(hey)
     print (hey)
     print (hi)
+    print ("you are cool")
     print(hi , bool(hey<35) )
 
-=======
-def nagasaki (hi):
-    hey = 3
-    hey = hey + 32 + 0.2
-    hey = int(hey)
-    print (hey)
-    print (hi)
-    print(hi , bool(hey<35) )
-
->>>>>>> origin/main
 nagasaki ("meowmeow") 
